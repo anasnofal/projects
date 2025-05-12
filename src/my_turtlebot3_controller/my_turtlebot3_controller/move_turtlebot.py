6b84a915a9736f31548a4bbdaf4015a2a6235736
anasnofal <anasnofal14@yahoo.com>
2025-05-12 10:04:03 +0200

    fix

--- src/my_turtlebot3_controller/my_turtlebot3_controller/move_turtlebot.py
+++ src/my_turtlebot3_controller/my_turtlebot3_controller/move_turtlebot.py
@@ -2,23 +2,18 @@
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
-
-class MoveTurtleBot3(Node):
-
+class CmdVelRelay(Node):
     def __init__(self):
-        super().__init__('move_turtlebot')
-        self.publisher = self.create_publisher(Twist, '/cmd_vel', 10)
-        self.timer = self.create_timer(0.1, self.timer_callback)
-        self.cmd = Twist()
+        super().__init__('cmd_vel_relay')
+        self.sub = self.create_subscription(Twist, '/gazebo/cmd_vel', self.callback, 10)
+        self.pub = self.create_publisher(Twist, '/cmd_vel', 10)
 
-    def timer_callback(self):
-        self.cmd.linear.x = 0.2  # Move forward at 0.2 m/s
-        self.publisher.publish(self.cmd)
-        self.get_logger().info('Moving forward...')
+    def callback(self, msg):
+        self.pub.publish(msg)
 
-def main(args=None):
-    rclpy.init(args=args)
-    node = MoveTurtleBot3()
+def main():
+    rclpy.init()
+    node = CmdVelRelay()
     rclpy.spin(node)
     node.destroy_node()
     rclpy.shutdown()
