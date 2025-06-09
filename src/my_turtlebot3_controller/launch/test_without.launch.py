9b1e4959e4ceaf12cdedb05a8379d46ba326ce90
anasnofal <anasnofal14@yahoo.com>
2025-06-09 16:22:10 +0200

    fix

--- src/my_turtlebot3_controller/launch/test_without.launch.py
+++ src/my_turtlebot3_controller/launch/test_without.launch.py
@@ -3,17 +3,15 @@ import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
-from launch.launch_description_sources import PythonLaunchDescriptionSource 
 from launch_ros.actions import Node
 
 def generate_launch_description():
-    my_controller_pkg_dir = get_package_share_directory('my_turtlebot3_controller')
-    nav2_bringup_pkg_dir = get_package_share_directory('nav2_bringup')
-    slam_toolbox_pkg_dir = get_package_share_directory('slam_toolbox')
-
     turtlebot3_gazebo_pkg_dir = get_package_share_directory('turtlebot3_gazebo')
-
+    my_controller_pkg_dir = get_package_share_directory('my_turtlebot3_controller')
+    slam_toolbox_pkg_dir = get_package_share_directory('slam_toolbox')
+    nav2_bringup_pkg_dir = get_package_share_directory('nav2_bringup')
 
     use_sim_time = LaunchConfiguration('use_sim_time', default='true')
     
@@ -23,47 +21,65 @@ def generate_launch_description():
     rviz_config_file = LaunchConfiguration('rviz_config',
         default=os.path.join(nav2_bringup_pkg_dir, 'rviz', 'nav2_default_view.rviz'))
 
-    return LaunchDescription([
+    declare_use_sim_time_cmd = DeclareLaunchArgument(
+        'use_sim_time',
+        default_value='true',
+        description='Use simulation (Gazebo) clock if true')
+        
+    declare_slam_params_file_cmd = DeclareLaunchArgument(
+        'slam_params_file',
+        default_value=slam_params_file,
+        description='Full path to the SLAM parameters file to use')
 
-        DeclareLaunchArgument(
-            'use_sim_time',
-            default_value='true',
-            description='Use simulation (Gazebo) clock if true'),
+    declare_rviz_config_file_cmd = DeclareLaunchArgument(
+        'rviz_config',
+        default_value=rviz_config_file,
+        description='Full path to the RViz configuration file to use')
 
-
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(
-                os.path.join(turtlebot3_gazebo_pkg_dir, 'launch', 'turtlebot3_world.launch.py')
-            ),
-            launch_arguments={'use_sim_time': use_sim_time}.items(),
+    start_gazebo_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(turtlebot3_gazebo_pkg_dir, 'launch', 'turtlebot3_world.launch.py')
         ),
-
-        # 2. Launch SLAM Toolbox
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(
-                os.path.join(slam_toolbox_pkg_dir, 'launch', 'online_async_launch.py')),
-            launch_arguments={
-                'use_sim_time': use_sim_time,
-                'slam_params_file': slam_params_file
-            }.items(),
+        launch_arguments={'use_sim_time': use_sim_time}.items(),
+    )
+    
+    start_slam_toolbox_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(slam_toolbox_pkg_dir, 'launch', 'online_async_launch.py')
         ),
+        launch_arguments={
+            'use_sim_time': use_sim_time,
+            'slam_params_file': slam_params_file
+        }.items(),
+    )
 
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(
-                os.path.join(nav2_bringup_pkg_dir, 'launch', 'navigation_launch.py')),
-            launch_arguments={
-                'use_sim_time': use_sim_time,
-                'map': ''  
-            }.items(),
+    start_rviz_cmd = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        arguments=['-d', rviz_config_file],
+        parameters=[{'use_sim_time': use_sim_time}],
+        output='screen')
+
+    start_nav2_bringup_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(nav2_bringup_pkg_dir, 'launch', 'navigation_launch.py')
         ),
+        launch_arguments={
+            'use_sim_time': use_sim_time,
+            'params_file': slam_params_file
+        }.items(),
+    )
 
+    ld = LaunchDescription()
+    
+    ld.add_action(declare_use_sim_time_cmd)
+    ld.add_action(declare_slam_params_file_cmd)
+    ld.add_action(declare_rviz_config_file_cmd)
+    
+    ld.add_action(start_gazebo_cmd)
+    ld.add_action(start_slam_toolbox_cmd)
+    ld.add_action(start_rviz_cmd)
+    ld.add_action(start_nav2_bringup_cmd)
 
-        Node(
-            package='rviz2',
-            executable='rviz2',
-            name='rviz2',
-            arguments=['-d', rviz_config_file],
-            output='screen',
-            parameters=[{'use_sim_time': use_sim_time}]
-        ),
-    ])
+    return ld
