20b6466ff8b91f5c59f8b912461a470289133ec7
anasnofal <anasnofal14@yahoo.com>
2025-06-09 15:50:19 +0200

    import missing lib

--- src/my_turtlebot3_controller/launch/test_without.launch.py
+++ src/my_turtlebot3_controller/launch/test_without.launch.py
@@ -4,6 +4,7 @@ from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.substitutions import LaunchConfiguration
+from launch.launch_description_sources import PythonLaunchDescriptionSource 
 from launch_ros.actions import Node
 
 def generate_launch_description():
